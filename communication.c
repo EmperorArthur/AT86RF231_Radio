d0d13952d4bd9cd447773fc35bee5734d604f6b8
Arthur Moore <Arthur.Moore@cd-net.net>
2013-02-01 16:11:11 -0600

    Communication module now uses it's own internal functions.
    This more easily allows piping cout/cin to/from multiple sources.

--- communication.c
+++ communication.c
@@ -5,21 +5,37 @@
 #include <avr/io.h>
 #include "uart.h"
 
-#ifdef __cplusplus
-extern "C"{
-	FILE * com_stream;
+int com_getchar_f(FILE *stream){
+	return (int) com_getchar();
 }
+unsigned char com_getchar(){
+	return (unsigned char) uart_getchar(1);
+}
+int com_putchar_f(char outChar,FILE *stream){
+	com_putchar(outChar);
+	return 0;
+}
+void com_putchar(unsigned char outChar){
+	//Serial expects crlf instead of just lf
+	if('\n' == outChar){
+		com_putchar('\r');
+	}
+	uart_putchar(outChar);
+}
+
+#ifdef __cplusplus
+	extern "C"{
+		FILE * com_stream;
+	}
 #else
-static FILE com_stream = FDEV_SETUP_STREAM(
-	uart_putchar_f,
-	uart_getchar_f,
-	_FDEV_SETUP_RW
-);
+	static FILE com_stream = FDEV_SETUP_STREAM(
+		com_putchar_f,
+		com_getchar_f,
+		_FDEV_SETUP_RW
+	);
 #endif
 
-
-
-//Set up both the UART and the Radio
+//Set up the serial connection
 int communication_setup(){
 	// UART
 	DDRD |= (1<<PD1);
@@ -27,12 +43,12 @@ int communication_setup(){
 	uart_setup();
 	
 	#ifdef __cplusplus
-	com_stream = fdevopen(
-		uart_putchar_f,
-		uart_getchar_f);
-	stdout = stdin = com_stream;
+		com_stream = fdevopen(
+			com_putchar_f,
+			com_getchar_f);
+		stdout = stdin = com_stream;
 	#else
-	stdout = &com_stream;
+		stdout = &com_stream;
 	#endif
 	return 0;
 }
--- communication.h
+++ communication.h
@@ -4,6 +4,13 @@
 #ifndef COMMUNICATION_H
 #define COMMUNICATION_H
 
+#include <stdio.h>
+
 int communication_setup();				//Set up Serial communication
+
+int com_getchar_f(FILE *stream);
+unsigned char com_getchar();
+int com_putchar_f(char outChar,FILE *stream);
+void com_putchar(unsigned char outChar);
 
 #endif
